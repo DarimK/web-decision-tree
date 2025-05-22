b1ae1212c49b8ed5a5be7a5f062562f2a7f48b61
Darim Khan <61602935+DarimK@users.noreply.github.com>
2025-05-21 23:58:18 -0400

    Update Graphics class #2

--- Graphics.js
+++ Graphics.js
@@ -1,8 +1,11 @@
 class Graphics {
+    static MAX_Z_INDEX = 99;
+
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
         this.figures = {};
+        this.zIndexLayers = [];
         this.camera = { x: 0, y: 0, zoom: 1 };
         this.nextId = 0;
     }
@@ -15,29 +18,41 @@ class Graphics {
 
     #add(data) {
         this.figures[this.nextId] = data;
+        data.zIndex = data.zIndex > Graphics.MAX_Z_INDEX ? Graphics.MAX_Z_INDEX : data.zIndex;
+        if (this.zIndexLayers[data.zIndex]) {
+            this.zIndexLayers[data.zIndex].push(this.nextId);
+        } else {
+            this.zIndexLayers[data.zIndex] = [this.nextId];
+        }
         return this.nextId++;
     }
 
     remove(id) {
+        const zIndex = this.figures[id].zIndex;
+        this.zIndexLayers[zIndex].splice(this.zIndexLayers[zIndex].indexOf(id), 1);
+        if (this.zIndexLayers[zIndex].length === 0) {
+            this.zIndexLayers.splice(zIndex, 1);
+        }
         delete this.figures[id];
     }
 
     reset() {
         this.figures = {};
+        this.zIndexLayers = [];
         this.nextId = 0;
         this.setCamera(0, 0, 1);
     }
 
-    addRectangle(x, y, width, height, borderWidth = 0, color = "black", borderColor = "black") {
-        return this.#add({ type: "rect", x, y, width, height, borderWidth, color, borderColor });
+    addRectangle(x, y, width, height, borderWidth = 0, color = "black", borderColor = "black", zIndex = 0) {
+        return this.#add({ type: "rect", x, y, width, height, borderWidth, color, borderColor, zIndex });
     }
 
-    addCircle(x, y, radius, borderWidth = 0, color = "black", borderColor = "black") {
-        return this.#add({ type: "circle", x, y, radius, borderWidth, color, borderColor });
+    addCircle(x, y, radius, borderWidth = 0, color = "black", borderColor = "black", zIndex = 0) {
+        return this.#add({ type: "circle", x, y, radius, borderWidth, color, borderColor, zIndex });
     }
 
-    addLine(x1, y1, x2, y2, width, color = "black") {
-        return this.#add({ type: "line", x1, y1, x2, y2, width, color });
+    addLine(x1, y1, x2, y2, width, color = "black", zIndex = 0) {
+        return this.#add({ type: "line", x1, y1, x2, y2, width, color, zIndex });
     }
 
     update() {
@@ -47,42 +62,44 @@ class Graphics {
         ctx.translate(-camera.x * camera.zoom + this.canvas.width / 2, -camera.y * camera.zoom + this.canvas.height / 2);
         ctx.scale(camera.zoom, camera.zoom);
 
-        for (const id in this.figures) {
-            const figure = this.figures[id];
-            ctx.fillStyle = figure.color;
-            ctx.strokeStyle = figure.borderColor ?? figure.color;
+        for (const layer of this.zIndexLayers) {
+            for (const id of layer) {
+                const figure = this.figures[id];
+                ctx.fillStyle = figure.color;
+                ctx.strokeStyle = figure.borderColor ?? figure.color;
 
-            if (figure.type === "rect") {
-                ctx.fillRect(figure.x - figure.width / 2, figure.y - figure.height / 2, figure.width, figure.height);
+                if (figure.type === "rect") {
+                    ctx.fillRect(figure.x - figure.width / 2, figure.y - figure.height / 2, figure.width, figure.height);
 
-                if (figure.borderWidth > 0) {
-                    const bw = figure.borderWidth;
-                    const halfBW = bw / 2;
-                    ctx.lineWidth = figure.borderWidth;
-                    ctx.strokeRect(
-                        figure.x + halfBW - figure.width / 2,
-                        figure.y + halfBW - figure.height / 2,
-                        figure.width - bw,
-                        figure.height - bw
-                    );
-                }
-            } else if (figure.type === "circle") {
-                ctx.beginPath();
-                ctx.arc(figure.x, figure.y, figure.radius, 0, Math.PI * 2);
-                ctx.fill();
-
-                if (figure.borderWidth > 0) {
-                    ctx.lineWidth = figure.borderWidth;
+                    if (figure.borderWidth > 0) {
+                        const bw = figure.borderWidth;
+                        const halfBW = bw / 2;
+                        ctx.lineWidth = figure.borderWidth;
+                        ctx.strokeRect(
+                            figure.x + halfBW - figure.width / 2,
+                            figure.y + halfBW - figure.height / 2,
+                            figure.width - bw,
+                            figure.height - bw
+                        );
+                    }
+                } else if (figure.type === "circle") {
                     ctx.beginPath();
-                    ctx.arc(figure.x, figure.y, figure.radius - figure.borderWidth / 2, 0, Math.PI * 2);
+                    ctx.arc(figure.x, figure.y, figure.radius, 0, Math.PI * 2);
+                    ctx.fill();
+
+                    if (figure.borderWidth > 0) {
+                        ctx.lineWidth = figure.borderWidth;
+                        ctx.beginPath();
+                        ctx.arc(figure.x, figure.y, figure.radius - figure.borderWidth / 2, 0, Math.PI * 2);
+                        ctx.stroke();
+                    }
+                } else if (figure.type === "line") {
+                    ctx.lineWidth = figure.width;
+                    ctx.beginPath();
+                    ctx.moveTo(figure.x1, figure.y1);
+                    ctx.lineTo(figure.x2, figure.y2);
                     ctx.stroke();
                 }
-            } else if (figure.type === "line") {
-                ctx.lineWidth = figure.width;
-                ctx.beginPath();
-                ctx.moveTo(figure.x1, figure.y1);
-                ctx.lineTo(figure.x2, figure.y2);
-                ctx.stroke();
             }
         }
 
