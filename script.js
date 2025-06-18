bddd2d5e71cebb456df1ba70779adc7ff03f18e8
Darim Khan <61602935+DarimK@users.noreply.github.com>
2025-06-18 14:32:31 -0400

    Update graphics touch controls #7

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ let initialPinchDistance = 1;
 let initialZoom = 1;
 let lastTouchCount = 0;
 const div = document.createElement("div");
-div.textContent = "6";
+div.textContent = "7";
 document.body.appendChild(div);
 function showError(callback) {
     try {
@@ -124,16 +124,17 @@ function doubleTouchStart(event) {
 // }, { passive: false });
 canvas.addEventListener("touchend", (event) => {
     if (event.touches.length === 0) {
-        isDragging = false;
+        // isDragging = false;
     } else if (event.touches.length === 1) {
         lastX = event.touches[0].clientX;
         lastY = event.touches[0].clientY;
+        div.textContent = "amogus";
     }
     lastTouchCount = event.touches.length;
 });
 canvas.addEventListener("touchmove", (event) => {
     if (event.touches.length > lastTouchCount) {
-        isDragging = true;
+        // isDragging = true;
         if (event.touches.length >= 2) {
             doubleTouchStart(event);
         } else {
@@ -152,14 +153,14 @@ canvas.addEventListener("touchmove", (event) => {
             currentX = (event.touches[0].clientX + event.touches[1].clientX) / 2;
             currentY = (event.touches[0].clientY + event.touches[1].clientY) / 2;
         }
-        if (isDragging) {
+        // if (isDragging) {
             const dx = currentX - lastX;
             const dy = currentY - lastY;
             cameraX -= dx / zoom;
             cameraY -= dy / zoom;
             lastX = currentX;
             lastY = currentY;
-        }
+        // }
     }
     lastTouchCount = event.touches.length;
     event.preventDefault();
